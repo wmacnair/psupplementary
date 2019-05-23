3126733dc608afdf62d6a43dc1f9f44fa0bc4e76
Will Macnair <macnairw@dhcp-wlan-uzh-10-12-15-163.uzh.ch>
2019-05-23 18:11:52 +0200

    completed documentation of data, plus another couple of bits of documentation

--- R/data.R
+++ R/data.R
@@ -1,10 +1,10 @@
-#' Aging acinar cells
+#' Aging human acinar cells
 #'
 #' The variable 'donor_age' (age of donor in years) was used as sequential labels. Taken from publication 
 #' "Single-Cell Analysis of Human Pancreas Reveals Transcriptional Signatures of Aging and Somatic Mutation Patterns"
 #' GSE81547
 #'
-#' @format A SingleCellExperiment object with [genes] by [cells].
+#' @format A SingleCellExperiment object with 23368 genes by 411 cells.
 #' @source \url{https://www.sciencedirect.com/science/article/pii/S009286741731053X}
 "acinar_sce"
 
@@ -14,17 +14,17 @@
 #' "Single-Cell RNA-Seq Analysis Maps Development of Human Germline Cells and Gonadal-Niche Interactions"
 #' GSE86146
 #'
-#' @format A SingleCellExperiment object with [genes] by [cells].
+#' @format A SingleCellExperiment object with 24153 genes by 992 cells.
 #' @source \url{https://www.cell.com/cell-stem-cell/fulltext/S1934-5909(17)30078-4}
 "germ_sce"
 
-#' Developing beta cells
+#' Developing mouse beta cells
 #'
 #' The variable 'age' (developmental stage) was used as sequential labels. Taken from publication 
 #' "Deciphering Pancreatic Islet β Cell and α Cell Maturation Pathways and Characteristic Features at the Single-Cell Level"
 #' GSE87375
 #'
-#' @format A SingleCellExperiment object with [genes] by [cells].
+#' @format A SingleCellExperiment object with 40737 genes by 575 cells.
 #' @source \url{https://www.cell.com/cell-metabolism/fulltext/S1550-4131(17)30208-5}
 "beta_sce"
 
@@ -34,26 +34,26 @@
 #' "Single-cell RNA-seq reveal lineage formation and X-chromosome dosage compensation in human preimplantation embryos"
 #' E-MTAB-3929
 #'
-#' @format A SingleCellExperiment object with [genes] by [cells].
+#' @format A SingleCellExperiment object with 26178 genes by 1529 cells.
 #' @source \url{https://www.ebi.ac.uk/arrayexpress/experiments/E-MTAB-3929/}
 "hesc_sce"
 
-#' MEFs reprogrammed to neurons
+#' Mouse embryonic fibroblasts reprogrammed to neurons
 #'
 #' The variable 'time_point' (days since induction) was used as sequential labels. Taken from publication 
 #' "Dissecting direct reprogramming from fibroblast to neuron using single-cell RNA-seq"
 #' GSE67310
 #'
-#' @format A SingleCellExperiment object with [genes] by [cells].
+#' @format A SingleCellExperiment object with 22524 genes by 315 cells.
 #' @source \url{https://www.nature.com/articles/nature18323}
 "mef_sce"
 
-#' [human?] colon cells
+#' Mouse colon cells
 #'
 #' The sequential labels were derived from unsupervised clustering, using Seurat. Taken from publication 
 #' "Unsupervised Trajectory Analysis of Single-Cell RNA-Seq and Imaging Data Reveals Alternative Tuft Cell Origins in the Gut"
 #' GSE102698
 #'
-#' @format A SingleCellExperiment object with [genes] by [cells].
+#' @format A SingleCellExperiment object with 25504 genes by 1894 cells.
 #' @source \url{https://www.cell.com/cell-systems/fulltext/S2405-4712(17)30449-0}
 "colon_sce"
